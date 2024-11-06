8cad17035d3bfd544d9df5070c49d1a1e079c550
Omniaahmedm <80421868+Omniaahmedm@users.noreply.github.com>
2024-11-06 09:03:50 +0300

    Update home.py

--- home.py
+++ home.py
@@ -42,4 +42,12 @@ if selected == "Overview":
    st.write('University Of Khartoum,Khartoum,Sudan')
    st.subheader('Certifications:')
    st.write('IBM Data Science Professional Cerificate – Coursera | 2023')
-
+elif selected == "Projects":
+  col1, col2, col3 = st.columns([1, 1, 1])
+     with col1:
+        st.image("https://miro.medium.com/max/737/1*Xap6OxaZvD7C7eMQKkaHYQ.jpeg", caption="BProject1")
+        st.markdown("[Link]()")
+     with col2:
+        st.image("https://miro.medium.com/max/737/1*Xap6OxaZvD7C7eMQKkaHYQ.jpeg", caption="Project2") 
+        st.markdown("[Link]()")
+    
