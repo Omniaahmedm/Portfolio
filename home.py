b7b4ca5ad7222a17837ff640522a9ade5e91d44d
Omniaahmedm <80421868+Omniaahmedm@users.noreply.github.com>
2024-11-08 21:41:02 +0300

    Update home.py

--- home.py
+++ home.py
@@ -44,8 +44,43 @@ if selected == "Overview":
    st.write('IBM Data Science Professional Cerificate – Coursera | 2023')
 elif selected == "Projects":
   col1, col2, col3 = st.columns(3)
-  with col1:
-    st.image("https://miro.medium.com/v2/resize:fit:828/format:webp/0*4oNUgPOA69JyeVwL.jpg", caption="Suspicious Web Threat Interactions")
+ 
+  st.image("https://miro.medium.com/v2/resize:fit:828/format:webp/0*4oNUgPOA69JyeVwL.jpg", caption="Suspicious Web Threat Interactions")
+  st.write('')
+  st.write('''This Python-based pipeline is designed for web traffic analysis and threat detection, aimed at identifying suspicious or malicious interactions within web application traffic. The goal is to flag anomalous behaviors such as data exfiltration, brute-force attacks, DoS attacks, and geographic anomalies that may indicate potential cybersecurity threats. The pipeline performs several stages of data processing, feature engineering, and machine learning model training to detect malicious activity.
+
+Key Steps in the Pipeline:
+Data Preprocessing:
+
+Datetime Conversion: Converts timestamps (e.g., creation_time, end_time) to datetime format to calculate session durations.
+Missing Value Handling: Fills missing values in the dataset with zeros or other appropriate strategies.
+Feature Engineering:
+
+Aggregates web traffic data by source IP address (src_ip) and source IP country code (src_ip_country_code).
+Flags suspicious IP addresses with high outgoing traffic (bytes_out) or multiple failed requests (HTTP 403 and 404).
+Identifies potential exfiltration activities by comparing outbound to inbound traffic on common ports (HTTP/HTTPS).
+Detects brute-force or DoS attempts based on a high number of requests and failed login attempts (403 errors).
+Threat Detection:
+
+Flags suspicious countries based on high traffic volume or failed requests.
+Flags suspicious IP addresses or countries based on quantile thresholds (top 5% of traffic volume or failed requests).
+Exfiltration detection based on the disproportionate amount of data sent compared to received data.
+Brute-force and DoS detection based on abnormal request rates, particularly from specific source IP addresses.
+Machine Learning Model:
+
+XGBoost is used to classify whether a session is suspicious or not, using aggregated features (e.g., total bytes transferred, failed requests, request count).
+The model is trained on the feature set, and performance is evaluated using accuracy, classification reports, and confusion matrices.
+Evaluation:
+
+Performance metrics like accuracy, precision, recall, and F1-score are used to evaluate the effectiveness of the model.
+The SHAP library is employed for model interpretability, explaining the importance of features in predicting suspicious activities.''')
+    st.write('Skills:')
+    st.write('''- Data Preprocessing & Cleaning  
+                - Feature Engineering      
+                - Machine Learning & Model Training   
+                - Evaluation & Interpretability ''') 
+    st.write('Tools:')
+    st.write('Pandas , NumPy,  Matplotlib / Seaborn, Scikit-learn, XGBoost ')
     st.markdown("[Link](https://portfolio-project1.streamlit.app/)")
  
     
